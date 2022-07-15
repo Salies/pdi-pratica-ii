a920e56e463e91e1ab8e478b2fcc30e4fe02322a
Salies <serezane@protonmail.com>
2022-07-14 22:34:13 -0300

    mouse event

--- pdi/dct/gui/mainwindow.py
+++ pdi/dct/gui/mainwindow.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QLabel, QPushButt
 from PyQt5.QtGui import QPixmap, QImage
 # esse Qt é o namespace do Qt, com algumas propriedades (números, valores) predefinidos
 # por exemplo o Qt.AlignVCenter em C++ seria Qt::AlignVCenter
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSignal
 from PIL import Image
 from PIL.ImageQt import ImageQt
 from numpy import array as nparray
@@ -21,15 +21,25 @@ class MainWindow(QMainWindow):
         # Criando menuBar
         menubar = self.menuBar()
         menuArquivo = QMenu("Arquivo", self)
+        menuOp = QMenu("Operações", self)
+        pb_action = QAction("Filtro passa-baixa", self)
+        pa_action = QAction("Filtro passa-alta", self)
+        pb_action.triggered.connect(self.fazer_passa_baixa)
+        pa_action.triggered.connect(self.fazer_passa_alta)
         abrir_action = QAction("Abrir", self)
         abrir_action.triggered.connect(self.abrir)
         menuArquivo.addAction(abrir_action)
+        menuOp.addAction(pb_action)
+        menuOp.addAction(pa_action)
         menubar.addMenu(menuArquivo)
+        menubar.addMenu(menuOp)
         # Criando statusBar
         self.statusBar().showMessage("Abra uma imagem")
         # Criando labels pras images
         self.__label1 = QLabel()
-        self.__label2 = QLabel()
+        #self.__label2 = QLabel()
+        self.__label2 = LabelComTracking()
+        self.__label2.clicouSignal.connect(self.pintar_pixel)
         self.__label1.setFixedSize(130, 130)
         self.__label2.setFixedSize(130, 130)
         self.__label1.setFrameStyle(QFrame.StyledPanel)
@@ -71,12 +81,36 @@ class MainWindow(QMainWindow):
         self.statusBar().showMessage("Calculando DCT. Aguarde...")
         QApplication.processEvents() # força a atualização da statusBar (senão ela atrasa)
         self.__C, dct_vmax, dct_vmin = dct(nparray(self.__image1))
-        self.__label2.setPixmap(QPixmap.fromImage(ImageQt(Image.fromarray(normalizar(self.__C, dct_vmax, dct_vmin)).convert("L"))))
+        self.__image2qt = ImageQt(Image.fromarray(normalizar(self.__C, dct_vmax, dct_vmin)).convert("L"))
+        self.__label2.setPixmap(QPixmap.fromImage(self.__image2qt))
         self.statusBar().showMessage("DCT concluído.")
 
     def fazer_idct(self, event):
         self.statusBar().showMessage("Calculando iDCT. Aguarde...")
         QApplication.processEvents()
-        self.__image1 = ImageQt(Image.fromarray(idct(self.__C)).convert('L'))
-        self.__label1.setPixmap(QPixmap.fromImage(self.__image1))
+        self.__image1 = Image.fromarray(idct(self.__C)).convert('L')
+        self.__label1.setPixmap(QPixmap.fromImage(ImageQt(self.__image1)))
         self.statusBar().showMessage("iDCT concluído.")
+
+    def fazer_passa_baixa(self, event):
+        print("passa-baixa")
+
+    def fazer_passa_alta(self, event):
+        print("passa-alta")
+
+    def pintar_pixel(self, pos):
+        print("pintar o pixel", pos)
+    
+# Label com mouse tracking, para podermos
+# pintar a posição onde o usuário clicar
+class LabelComTracking(QLabel):
+    clicouSignal = pyqtSignal(tuple)
+    def __init__(self):
+        super().__init__()
+        self.setMouseTracking(True)
+
+    #def mouseMoveEvent(self, event):
+    #    print("passando por cima")
+
+    def mousePressEvent(self, event):
+        self.clicouSignal.emit((event.x(), event.y()))
