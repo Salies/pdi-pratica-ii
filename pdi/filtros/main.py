cb54b8667b4e8acd961e6816f636f5ee18879236
Salies <serezane@protonmail.com>
2022-07-17 20:56:20 -0300

    Carrega imagem pro DearPyGui

--- pdi/filtros/main.py
+++ pdi/filtros/main.py
@@ -1,12 +1,23 @@
 import dearpygui.dearpygui as dpg
-import dearpygui.demo as demo
+from PIL import Image
+import numpy as np
+
+def save_callback():
+    print("Save Clicked")
 
 dpg.create_context()
-dpg.create_viewport(title='Custom Title', width=600, height=600)
-
-demo.show_demo()
-
+dpg.create_viewport()
 dpg.setup_dearpygui()
+im = Image.open("imagens/ramona.png").convert('L').convert('RGBA')
+data = np.frombuffer(im.tobytes(), dtype=np.uint8) / 255.0
+width, height = im.size
+
+with dpg.texture_registry():
+    texture_id = dpg.add_static_texture(width, height, data)
+
+with dpg.window(label="Example Window"):
+    dpg.add_image(texture_id)
+
 dpg.show_viewport()
 dpg.start_dearpygui()
 dpg.destroy_context()
