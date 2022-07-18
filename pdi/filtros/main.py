e99d666c98801a762ddab1c7a15a2756ddc05638
Salies <serezane@protonmail.com>
2022-07-17 22:10:24 -0300

    Filtros UI base

--- pdi/filtros/main.py
+++ pdi/filtros/main.py
@@ -1,23 +1,64 @@
-import dearpygui.dearpygui as dpg
+from sys import argv
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QFrame, QScrollArea, QMenu, QAction, QFileDialog, QSizePolicy
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import Qt
 from PIL import Image
-import numpy as np
+from PIL.ImageQt import ImageQt
+from pdi.sobre import Sobre
 
-def save_callback():
-    print("Save Clicked")
+class MainWindow(QMainWindow):
+    def __init__ (self):
+        super().__init__()
+        self.setWindowTitle("PDI -- Filtros")
+        self.setWindowIcon(QIcon('ico/enigma.ico'))
+        centralWidget = QWidget()
+        centralLayout = QHBoxLayout()
+        menuArquivo = QMenu("Arquivo", self)
+        menuAjuda = QMenu("Ajuda", self)
+        sobre_action = QAction("Sobre", self)
+        abrir_action = QAction("Abrir", self)
+        sobre_action.triggered.connect(lambda: Sobre('ico/enigma.ico'))
+        abrir_action.triggered.connect(self.abrir)
+        menuAjuda.addAction(sobre_action)
+        menuArquivo.addAction(abrir_action)
+        scroll1 = QScrollArea()
+        scroll2 = QScrollArea()
+        scroll1.setFixedSize(516, 516)
+        scroll2.setFixedSize(516, 516)
+        self.__label1 = QLabel()
+        self.__label2 = QLabel()
+        scroll1.setWidget(self.__label1)
+        scroll2.setWidget(self.__label2)
+        opcoesLayout = QVBoxLayout()
+        opcoesLayout.setAlignment(Qt.AlignVCenter)
+        opcoesLayout.addWidget(QLabel("Filtros:"))
+        self.__btnMinimo = QPushButton("Mínimo")
+        self.__btnMaximo = QPushButton("Máximo")
+        self.__btnPMedio = QPushButton("Ponto médio")
+        opcoesLayout.addWidget(self.__btnMinimo)
+        opcoesLayout.addWidget(self.__btnMaximo)
+        opcoesLayout.addWidget(self.__btnPMedio)
+        centralLayout.addWidget(scroll1)
+        centralLayout.addLayout(opcoesLayout)
+        centralLayout.addWidget(scroll2)
+        centralWidget.setLayout(centralLayout)
+        menubar = self.menuBar()
+        menubar.addMenu(menuArquivo)
+        menubar.addMenu(menuAjuda)
+        self.setCentralWidget(centralWidget)
 
-dpg.create_context()
-dpg.create_viewport()
-dpg.setup_dearpygui()
-im = Image.open("imagens/ramona.png").convert('L').convert('RGBA')
-data = np.frombuffer(im.tobytes(), dtype=np.uint8) / 255.0
-width, height = im.size
+    def abrir(self, event):
+        pathArquivo = QFileDialog.getOpenFileName(self, "Abrir imagem", "", "Imagens (*.png *.jpg *.bmp)")[0]
+        if not pathArquivo:
+            return
+        self.__image1 = Image.open(pathArquivo).convert('L')
+        width, height = self.__image1.size
+        self.__label1.resize(width, height)
+        self.__label1.setPixmap(QPixmap.fromImage(ImageQt(self.__image1)))
 
-with dpg.texture_registry():
-    texture_id = dpg.add_static_texture(width, height, data)
+app = QApplication(argv)
+app.setAttribute(Qt.AA_DisableWindowContextHelpButton)
+w = MainWindow()
 
-with dpg.window(label="Example Window"):
-    dpg.add_image(texture_id)
-
-dpg.show_viewport()
-dpg.start_dearpygui()
-dpg.destroy_context()
+w.show()
+app.exec()
