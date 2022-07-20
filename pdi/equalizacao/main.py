cdbcaa8aa98fff5a18289990dd80bb91dda22866
Salies <serezane@protonmail.com>
2022-07-20 18:54:31 -0300

    hsl - rgb

--- pdi/equalizacao/main.py
+++ pdi/equalizacao/main.py
@@ -1,6 +1,26 @@
-from PIL import Image
-from numpy import array as nparray
+from PIL import Image, ImageChops
+import numpy as np
+from pdi.slithice import rgb_para_hsl, hsl_para_rgb
 
 # Fig0638(a)(lenna_RGB).tif
-img = Image.open("imagens/Fig0638(a)(lenna_RGB).tif")
-img.show()
+img = Image.open("imagens/Fig0638(a)(lenna_RGB).tif").convert("RGB")
+img_array = np.array(img)
+img_hsl = np.empty(img_array.shape)
+width, height = img_array.shape[:2]
+for i in range(width):
+    for j in range(height):
+        r, g, b = img_array[i][j]
+        h, s, l = rgb_para_hsl(r, g, b)
+        img_hsl[i][j] = np.array([h, s, l])
+
+img_teste = np.empty(img_array.shape)
+for i in range(width):
+    for j in range(height):
+        h, s, l = img_hsl[i][j]
+        r, g, b = hsl_para_rgb(h, s, l)
+        img_teste[i][j] = np.array([r, g, b])
+
+print(img_teste.shape)
+iii = Image.fromarray((img_teste).astype(np.uint8))
+aaa = ImageChops.subtract(img, iii)
+print(np.linalg.norm(np.array(aaa)))
--- pdi/slithice.py
+++ pdi/slithice.py
@@ -100,8 +100,8 @@ def conv(img, kernel, k_width, k_height, v_max = -(np.inf), v_min = np.inf):
     return out, v_max, v_min # retorna o máximo e mínimo, pra normalizar depois
 
 @njit
-def gerar_histograma(img):
-    h = np.zeros(256)
+def gerar_histograma(img, n_tons):
+    h = np.zeros(n_tons)
 
     for i, j in np.ndindex(img.shape):
         h[img[i, j]] += 1
@@ -109,22 +109,81 @@ def gerar_histograma(img):
     return h
 
 @njit
-def eq_histograma_img(img):
+def eq_histograma_img(img, n_tons):
     out = img.copy()
     h = gerar_histograma(img)
 
     width, height = img.shape
     freq_acc = 0
-    lut = np.empty(256)
-    escala = 255.0 / (width * height)
+    lut = np.empty(n_tons)
+    escala = 240.0 / (width * height)
 
-    for i in range(256):
+    for i in range(n_tons):
         freq_acc += h[i]
         lut[i] = max(0,  (freq_acc * escala) - 1)
 
-    h = np.zeros(256)
+    h = np.zeros(n_tons)
     for i, j in np.ndindex(img.shape):
         out[i, j] = lut[out[i, j]]
         h[out[i, j]] += 1
 
     return out
+
+# Igualzinho na biblioteca original em C++
+# Mas essa aqui eu não arredondo, pra ter mais precisão
+# (na outra precisava arredondar pois precisava mostrar)
+# (fiz um teste e o HSL voltou perfeito, com 0.0 diferença entre as imagens)
+@njit
+def rgb_para_hsl(r, g, b):
+    rr = r / 255.0
+    gg = g / 255.0
+    bb = b / 255.0
+    cmax = max([ rr, gg, bb ])
+    cmin = min([ rr, gg, bb ])
+    ll = (cmax + cmin) / 2.0
+    if (cmax == cmin):
+        h = s = 0
+    # Calcula H e S
+    else:
+        delta = cmax - cmin;
+        # S está na escala de 0...1, converter p/ 240
+        s = (delta / (1.0 - np.abs((2 * ll) - 1.0))) * 240
+        if (cmax == rr):
+            hh = ((gg - bb) / delta) + (6.0 if gg < bb else 0.0) # (gg < bb ? 6.0 : 0.0)
+        elif (cmax == gg):
+            hh = ((bb - rr) / delta) + 2.0
+        elif (cmax == bb):
+            hh = ((rr - gg) / delta) + 4.0
+
+        if (hh < 0.0):
+            hh += 360.0
+        hh *= 60.0
+        # 0 <= h < 360 => 0 <= h < 240
+        hh = (hh * 2.0) / 3.0
+        h = hh
+    # Calcula L
+    l = ll * 240.0
+
+    return h, s, l
+
+@njit
+def hsl_f(n, h, s, l):
+    k = np.fmod((n + (h / 30.0)), 12.0)
+    return np.round((l - (s * min(l, 1.0 - l)) * max(-1.0, min([ k - 3.0, 9.0 - k, 1.0 ]))) * 255.0)
+
+@njit
+def hsl_para_rgb(h, s, l):
+    ll = l / 240.0
+    # Imagem sem saturação (cinza)
+    if (s == 0):
+        r = g = b = np.round(ll * 255.0)
+        return r, g, b
+
+    hh = (h / 2.0) * 3.0
+    ss = s / 240.0
+
+    r = hsl_f(0, hh, ss, ll)
+    g = hsl_f(8, hh, ss, ll)
+    b = hsl_f(4, hh, ss, ll)
+
+    return r, g, b
