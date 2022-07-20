7e77a142850781b8a4120d3efa6afd41087d89fa
Daniel Serezane <serezane@protonmail.com>
2022-07-20 00:05:48 -0300

    pseudocores

--- pdi/pseudocores/main.py
+++ pdi/pseudocores/main.py
@@ -1,38 +1,41 @@
 from PIL import Image
 from numpy import array as nparray, empty
+from numba import njit
 
+@njit
+def colorizar(img):
+    width, height = img.shape[:2]
 
+    # Loopando e aplicando pseudocores
+    subs = empty(3)
+    for i in range(width):
+        for j in range(height):
+            tom = img[i, j][0]
+            if(tom < 64):
+                subs[0] = 0
+                subs[1] = 0
+                subs[2] = tom * 4
+            elif(tom >= 64 and tom < 128):
+                subs[0] = 0
+                subs[1] = (tom - 64) * 4
+                subs[2] = 255
+            elif(tom >= 128 and tom < 192):
+                subs[0] = 0
+                subs[1] = 255
+                subs[2] = 255 - ((tom - 128) * 4)
+            elif(tom >= 192):
+                subs[0] = (tom - 192) * 4
+                subs[1] = 255
+                subs[2] = 0
+            img[i, j] = subs
 
 # Abrindo imagem e representando como matriz
 # Já fazemos a conversão de escala de cinza para RGB
-# (a imagem de mantém cinza, mas cara tom agora está em RGB e não em apenas um uchar)
+# (a imagem mantém-se cinza, mas cada tom agora está em uma cor RGB)
 img = nparray(Image.open("imagens/Fig0622(a)(tropical_rain_grayscale.tif").convert('RGB'))
-#print(img)
 
-width, height = img.shape[:2]
+colorizar(img)
 
-# Loopando e aplicando pseudocores
-subs = empty(3)
-for i in range(width):
-    for j in range(height):
-        cor = img[i, j][0]
-        if(cor < 64):
-            subs[0] = 0
-            subs[1] = 0
-            subs[2] = cor * 4
-        elif(cor >= 64 and cor < 128):
-            subs[0] = 0
-            subs[1] = (cor - 64) * 4
-            subs[2] = 255
-        elif(cor >= 128 and cor < 192):
-            subs[0] = 0
-            subs[1] = 255
-            subs[2] = 255 - ((cor - 128) * 4)
-        elif(cor >= 192):
-            subs[0] = (cor - 192) * 4
-            subs[1] = 255
-            subs[2] = 0
-        img[i, j] = subs
-
+# Remontando e exibindo a imagem após a colorização
 img = Image.fromarray(img)
 img.show() 
