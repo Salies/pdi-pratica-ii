12ee28f130631bbc69521d8e1b2eb2572a879b51
Salies <serezane@protonmail.com>
2022-07-27 03:31:54 -0300

    zhang suen bugado kkkkkkkkk

--- pdi/afinamento/main.py
+++ pdi/afinamento/main.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+from numba import njit
 
 '''
 for (int k = -1; k <= 1; k++) {
@@ -11,21 +12,98 @@ for (int k = -1; k <= 1; k++) {
 }
 '''
 
-# Retorna a posição dos vizinhos de um pixel
-def get_vizinhos(i, j, w, h):
-    v = []
-    for k in range (-1, 2):
-        for l in range(-1, 2):
-            if((k, l) != (0, 0) and 0 <= i + k and i + k < h and 0 <= j + l and j + l < w):
-                v.append((i + k, j + l))
-    return v
+@njit
+def zhang_suen(img):
+    out = img.copy()
+    # Não tem do while em Python =/
+    a = 0
+    while True:
+        if(a > 1000): break
+        print("it", a)
 
+        mar = np.zeros(img.shape, dtype=np.uint8)
+        # Ignorando as bordas da imagem
+        for i in range(1, img.shape[0] - 1):
+            for j in range(1, img.shape[1] - 1):
+                if(img[i, j] == 0):
+                    continue
+                p = [
+                    img[i - 1, j], # p[2] -- 0
+                    img[i - 1, j + 1], # p[3] -- 1
+                    img[i, j + 1], # p[4] -- 2
+                    img[i + 1, j + 1], # p[5] -- 3
+                    img[i + 1, j], # p[6] -- 4
+                    img[i + 1, j - 1], # p[7] -- 5
+                    img[i, j - 1], # p[8] -- 6
+                    img[i - 1, j - 1] # p[9] -- 7
+                ]
+                N = sum(p)
+                # Se N = 8 e img[i, j] = 1, não é pixel de borda
+                if(N == 8):
+                    continue
+                # "pontuação" do algoritmo -- se atingir 4, é marcado pra eliminação
+                # TODO: decompor em funções
+                score = 0
+                if(N >= 2 and N <= 6):
+                    score += 1
+                S = 0
+                for k in range(-1, 6): # tem que dar a volta
+                    if(p[k + 1] != p[k]):
+                        S += 1
+                if(S == 1):
+                    score += 1
+                if(p[0] * p[2] * p[4] == 0):
+                    score += 1
+                if(p[2] * p[4] * p[6] == 0):
+                    score += 1
+                if(score == 4):
+                    mar[i, j] = 255
+        out -= mar
+        mar = np.zeros(img.shape, dtype=np.uint8)
+        for i in range(1, img.shape[0] - 1):
+            for j in range(1, img.shape[1] - 1):
+                p = [
+                    out[i - 1, j], # p[2] -- 0
+                    out[i - 1, j + 1], # p[3] -- 1
+                    out[i, j + 1], # p[4] -- 2
+                    out[i + 1, j + 1], # p[5] -- 3
+                    out[i + 1, j], # p[6] -- 4
+                    out[i + 1, j - 1], # p[7] -- 5
+                    out[i, j - 1], # p[8] -- 6
+                    out[i - 1, j - 1] # p[9] -- 7
+                ]
+                # Se N = 8 e img[i, j] = 1, não é pixel de borda
+                if(N == 8 and img[i, j] == 255):
+                    continue
+                # "pontuação" do algoritmo -- se atingir 4, é marcado pra eliminação
+                # TODO: decompor em funções
+                score = 0
+                if(N >= 2 and N <= 6):
+                    score += 1
+                S = 0
+                for k in range(-1, 6): # tem que dar a volta
+                    if(p[k + 1] != p[k]):
+                        S += 1
+                if(S == 1):
+                    score += 1
+                if(p[0] * p[2] * p[6] == 0):
+                    score += 1
+                if(p[0] * p[4] * p[6] == 0):
+                    score += 1
+                if(score == 4):
+                    mar[i, j] = 255
+        out -= mar
+        # Não há mais pixels eliminados
+        print(mar)
+        if(np.sum(mar) == 0):
+            break
+        a += 1
+    return out
+    
 # Abrindo como imagem binária
 # na pillow:
 #   False   :   pixel preto
 #   True    :   pixel branco
-#im = Image.open("imagens/teste.bmp").convert('1')
-#print(np.array(im))
-
-a = get_vizinhos(4, 4, 5, 5)
-print(a)
+im = Image.open("imagens/teste.bmp").convert('L')
+o = Image.fromarray(zhang_suen(np.array(im)))
+o.show()
