b86e9765531f5e889d7d85b0f2500b4c41a977a7
Salies <serezane@protonmail.com>
2022-07-27 18:51:44 -0300

    zhang-suen

--- pdi/afinamento/main.py
+++ pdi/afinamento/main.py
@@ -13,97 +13,78 @@ for (int k = -1; k <= 1; k++) {
 '''
 
 @njit
+def vizinhos(i, j, img):
+    return [
+            img[i - 1, j], # p[2] -- 0
+            img[i - 1, j + 1], # p[3] -- 1
+            img[i, j + 1], # p[4] -- 2
+            img[i + 1, j + 1], # p[5] -- 3
+            img[i + 1, j], # p[6] -- 4
+            img[i + 1, j - 1], # p[7] -- 5
+            img[i, j - 1], # p[8] -- 6
+            img[i - 1, j - 1] # p[9] -- 7
+    ]
+
+# Não vou jitar porque esse truque do Python é muito conveniente
+# e buga de eu jitar
+@njit
+def S(p):
+    aux = np.append(p, p[0]) # coloca o p2 no final
+    s = 0
+    for k in range(len(aux) - 1):
+        if (aux[k], aux[k + 1]) == (False, True):
+            s += 1
+    return s
+
+@njit
 def zhang_suen(img):
     out = img.copy()
-    # Não tem do while em Python =/
-    a = 0
-    while True:
-        if(a > 1000): break
-        print("it", a)
-
-        mar = np.zeros(img.shape, dtype=np.uint8)
-        # Ignorando as bordas da imagem
+    mar_p1 = [(0, 0)] # pixels marcados para remoção, passo 1 -- não há do while em Python
+    mar_p2 = [(0, 0)]
+    # loopa enquanto houverem pixels marcados em cada um dos passos
+    while len(mar_p1) != 0 or len(mar_p2) != 0:
+        # Passo 1
+        mar_p1 = []
+        # Ignora pixels na borda da imagem
         for i in range(1, img.shape[0] - 1):
             for j in range(1, img.shape[1] - 1):
-                if(img[i, j] == 0):
+                if(out[i, j] == False): # pixels pretos não podem ser de borda
                     continue
-                p = [
-                    img[i - 1, j], # p[2] -- 0
-                    img[i - 1, j + 1], # p[3] -- 1
-                    img[i, j + 1], # p[4] -- 2
-                    img[i + 1, j + 1], # p[5] -- 3
-                    img[i + 1, j], # p[6] -- 4
-                    img[i + 1, j - 1], # p[7] -- 5
-                    img[i, j - 1], # p[8] -- 6
-                    img[i - 1, j - 1] # p[9] -- 7
-                ]
-                N = sum(p)
-                # Se N = 8 e img[i, j] = 1, não é pixel de borda
-                if(N == 8):
+                p = p2, p3, p4, p6, p6, p7, p8, p9 = vizinhos(i, j, out)
+                if(sum(p) == 8): # se tem 8 vizinhos brancos não é pixel de borda
                     continue
-                # "pontuação" do algoritmo -- se atingir 4, é marcado pra eliminação
-                # TODO: decompor em funções
-                score = 0
-                if(N >= 2 and N <= 6):
-                    score += 1
-                S = 0
-                for k in range(-1, 6): # tem que dar a volta
-                    if(p[k + 1] != p[k]):
-                        S += 1
-                if(S == 1):
-                    score += 1
-                if(p[0] * p[2] * p[4] == 0):
-                    score += 1
-                if(p[2] * p[4] * p[6] == 0):
-                    score += 1
-                if(score == 4):
-                    mar[i, j] = 255
-        out -= mar
-        mar = np.zeros(img.shape, dtype=np.uint8)
+                # Se batarem as condições, marcar para eliminação
+                if (2 <= sum(p) <= 6 and S(p) == 1 and p2 * p4 * p6 == 0 and p4 * p6 * p8 == 0):
+                    mar_p1.append((i, j))
+        # Elimina os pixels
+        for i, j in mar_p1:
+            out[i, j] = False
+        # Passo 2
+        mar_p2 = []
         for i in range(1, img.shape[0] - 1):
             for j in range(1, img.shape[1] - 1):
-                p = [
-                    out[i - 1, j], # p[2] -- 0
-                    out[i - 1, j + 1], # p[3] -- 1
-                    out[i, j + 1], # p[4] -- 2
-                    out[i + 1, j + 1], # p[5] -- 3
-                    out[i + 1, j], # p[6] -- 4
-                    out[i + 1, j - 1], # p[7] -- 5
-                    out[i, j - 1], # p[8] -- 6
-                    out[i - 1, j - 1] # p[9] -- 7
-                ]
-                # Se N = 8 e img[i, j] = 1, não é pixel de borda
-                if(N == 8 and img[i, j] == 255):
+                if(out[i, j] == False):
                     continue
-                # "pontuação" do algoritmo -- se atingir 4, é marcado pra eliminação
-                # TODO: decompor em funções
-                score = 0
-                if(N >= 2 and N <= 6):
-                    score += 1
-                S = 0
-                for k in range(-1, 6): # tem que dar a volta
-                    if(p[k + 1] != p[k]):
-                        S += 1
-                if(S == 1):
-                    score += 1
-                if(p[0] * p[2] * p[6] == 0):
-                    score += 1
-                if(p[0] * p[4] * p[6] == 0):
-                    score += 1
-                if(score == 4):
-                    mar[i, j] = 255
-        out -= mar
-        # Não há mais pixels eliminados
-        print(mar)
-        if(np.sum(mar) == 0):
-            break
-        a += 1
+                p = p2, p3, p4, p6, p6, p7, p8, p9 = vizinhos(i, j, out)
+                if(sum(p) == 8):
+                    continue
+                if (2 <= sum(p) <= 6 and S(p) == 1 and p2 * p4 * p8 == 0 and p2 * p6 * p8 == 0):
+                    mar_p2.append((i, j))
+        for i, j in mar_p2:
+            out[i, j] = False
     return out
+
     
 # Abrindo como imagem binária
 # na pillow:
 #   False   :   pixel preto
 #   True    :   pixel branco
-im = Image.open("imagens/teste.bmp").convert('L')
-o = Image.fromarray(zhang_suen(np.array(im)))
-o.show()
+im = Image.open("imagens/teste.bmp").convert('1')
+#a = vizinhos(1, 1, np.array(im))
+#print(a)
+#print(a.dtype)
+#b = np.array([False, True, False, True, False, False, True, True, False])
+#print(S(b))
+'''o = Image.fromarray(zhang_suen(np.array(im)))
+o.show()'''
+Image.fromarray(zhang_suen(np.array(im))).show()
